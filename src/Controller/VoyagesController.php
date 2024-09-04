c838dc2a4bd5a5586689e84ca1bfd1a4ed44d247
safjhr <safiya@jaouahir.Com>
2024-09-04 15:24:47 +0200

    PHASE 9 : ORM (filtrage des enregistrement

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -19,7 +20,15 @@ class VoyagesController extends AbstractController
 {
     #[Route('/voyages', name: 'voyages')]
     public function index() : Response {
-        $visites = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("pages/voyages.html.twig",[
+                'visites' => $visites
+                ] );
+    }
+    
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre) : Response {
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
         return $this->render("pages/voyages.html.twig",[
                 'visites' => $visites
                 ] );
@@ -39,5 +48,13 @@ class VoyagesController extends AbstractController
         $this->repository = $repository;
     }
     
+    #[Route('/voyage/recherche/{champ}', name: 'voyages.findallequal')]
+    public function findAllEqual($champ, Request $request): Response{
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
    
 }
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -15,29 +15,38 @@ class VisiteRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Visite::class);
     }
-
-    //    /**
-    //     * @return Visite[] Returns an array of Visite objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('v')
-    //            ->andWhere('v.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('v.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Visite
-    //    {
-    //        return $this->createQueryBuilder('v')
-    //            ->andWhere('v.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * 
+     * @param type $champ
+     * @param type $ordre
+     * @return Visite[]
+     */
+    public function findAllOrderBy($champ, $ordre) : array{
+        return $this->createQueryBuilder('v')
+                ->orderBy('v.' .$champ, $ordre)
+                ->getQuery()
+                ->getResult();
+    }
+    /**
+     * 
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */
+    public function findByEqualValue($champ, $valeur): array{
+        if($valeur==""){
+            return $this->createQueryBuilder('v') // alias de la table
+                    ->orderBy('v.'.$champ, 'ASC')
+                    ->getQuery()
+                    ->getResult();
+            
+        }else{
+            return $this->createQueryBuilder('v')
+                    ->where('v.' .$champ. '=:valeur')
+                    ->setParameter('valeur', $valeur)
+                    ->orderBy('v.datecreation', 'DESC')
+                    ->getQuery()
+                    ->getResult();
+        }
+    }
 }
